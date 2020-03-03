fd7352d008c0b3bc19a28d07a3bf6caee04db99f
honey <honeykhra@gmail.com>
2020-03-03 21:31:24 +0530

    Updates

---
+++ precache-manifest.2a28faf34a29d7ab1410ca345af030f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31628fdf332a235b6472b04f17a2288",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1d9c5b7ca276dab25e96",
+    "url": "/monsters-rolodex/static/css/main.caf52875.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d9c5b7ca276dab25e96",
+    "url": "/monsters-rolodex/static/js/main.75427d53.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
